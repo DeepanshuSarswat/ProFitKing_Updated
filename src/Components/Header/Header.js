251bab9a704ff83cfc1391e9af7519b9dacd9e17
DEEPANSHU <>
2022-04-25 03:39:03 -0700

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+function Header() {
+  return (
+    <div className="Header">
+      <div className="Header_left">
+        <div></div>
+        <div></div>
+      </div>
+      <div className="Header_right">Deepanshu</div>
+    </div>
+  );
+}
+
+export default Header;
