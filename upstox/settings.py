2bb0b8aaeb5bfe3a1817582c09408a5e68a45176
hks74123 <hemant91852@gmail.com>
2022-05-25 17:33:41 +0530

    final_deploy

--- upstox/settings.py
+++ upstox/settings.py
@@ -142,7 +142,7 @@ CSRF_COOKIE_NAME = "X-CSRFToken"
 
 AUTH_USER_MODEL = "stocksapi.User1" 
 
-SITE_URL='https://profitking.herokuapp.com//'
+SITE_URL='https://profitking.herokuapp.com/'
 PASS_MAIL='74123586'
 MAIL_SENDER='no.reply.profitking@gmail.com'
 
