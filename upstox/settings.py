20aaabcceb2c2c2cdcfcbd2380c3a9cff3965b4e
hks74123 <hemant91852@gmail.com>
2022-05-25 14:15:00 +0530

    deploy

--- upstox/settings.py
+++ upstox/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-evmqwy$&dkhwd72d2sd7#%*l613f5uqoe!!nq8a(0l4frjwbhc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*','https://profitking.herokuapp.com/']
 
